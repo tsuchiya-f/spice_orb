965cf7d49dd92075813c4b6474f0a68af58a2a9b
Fuminori Tsuchiya <tsuchiya.f@tohoku.ac.jp>
2025-02-22 12:54:31 +0900

    add voyagers

--- lib/vex_spice_lib.py
+++ lib/vex_spice_lib.py
@@ -7,25 +7,25 @@ def spice_ini(source_dir='/Users/Shared/spice/'):
 
     # load SPK 
     # https://naif.jpl.nasa.gov/pub/naif/pds/data/vex-e_v-spice-6-v2.0/vexsp_2000/DATA/SPK/
-    spice.furnsh(source_dir + 'vex/kernel/spk/DE405.BSP')
-    spice.furnsh(source_dir + 'vex/kernel/spk/EARTHSTNS_FX_050714.BSP')
-    spice.furnsh(source_dir + 'vex/kernel/spk/EARTHSTNS_ITRF93_050714.BSP')
-    spice.furnsh(source_dir + 'vex/kernel/spk/ESTRACK_V03.BSP')
-    spice.furnsh(source_dir + 'vex/kernel/spk/ORVM_T19___________00001.BSP')
-    spice.furnsh(source_dir + 'vex/kernel/spk/OUTERPLANETS_V0003.BSP')
-    spice.furnsh(source_dir + 'vex/kernel/spk/VEX_STRUCT_V01.BSP')
+    spice.furnsh(source_dir + 'vex/kernels/spk/DE405.BSP')
+    spice.furnsh(source_dir + 'vex/kernels/spk/EARTHSTNS_FX_050714.BSP')
+    spice.furnsh(source_dir + 'vex/kernels/spk/EARTHSTNS_ITRF93_050714.BSP')
+    spice.furnsh(source_dir + 'vex/kernels/spk/ESTRACK_V03.BSP')
+    spice.furnsh(source_dir + 'vex/kernels/spk/ORVM_T19___________00001.BSP')
+    spice.furnsh(source_dir + 'vex/kernels/spk/OUTERPLANETS_V0003.BSP')
+    spice.furnsh(source_dir + 'vex/kernels/spk/VEX_STRUCT_V01.BSP')
 
     # load FK
     # https://naif.jpl.nasa.gov/pub/naif/pds/data/vco-v-spice-6-v1.0/vcosp_1000/data/fk/
-    spice.furnsh(source_dir + 'vex/kernel/fk/rssd0002.tf')
-    spice.furnsh(source_dir + 'vex/kernel/fk/vco_spacecraft_v26.tf')
+    spice.furnsh(source_dir + 'vex/kernels/fk/rssd0002.tf')
+    spice.furnsh(source_dir + 'vex/kernels/fk/vco_spacecraft_v26.tf')
 
     # load LSK 
     # https://naif.jpl.nasa.gov/pub/naif/pds/data/vex-e_v-spice-6-v2.0/vexsp_2000/DATA/LSK/
-    spice.furnsh(source_dir + 'vex/kernel/lsk/NAIF0011.TLS')
+    spice.furnsh(source_dir + 'vex/kernels/lsk/NAIF0011.TLS')
 
     # load PCK 
     # https://naif.jpl.nasa.gov/pub/naif/pds/data/vex-e_v-spice-6-v2.0/vexsp_2000/DATA/PCK/
-    spice.furnsh(source_dir + 'vex/kernel/pck/PCK00010.TPC')
+    spice.furnsh(source_dir + 'vex/kernels/pck/PCK00010.TPC')
 
     return
---
+++ lib/voyager_spice_lib.py
@@ -0,0 +1,38 @@
+import spiceypy as spice
+
+# ---------------------------------------------------------
+# Load NAIF SPICE kernels for S/C
+# ---------------------------------------------------------
+def spice_ini(source_dir='/Users/Shared/spice/'):
+
+    # load SPK 
+    # https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/
+    spice.furnsh(source_dir + 'generic_kernels//spk/planets/de442s.bsp ')
+
+    # https://naif.jpl.nasa.gov/pub/naif/VOYAGER/kernels/spk/
+    spice.furnsh(source_dir + 'voyagers/kernels/spk/Voyager_1.a54206u_V0.2_merged.bsp ')
+    spice.furnsh(source_dir + 'voyagers/kernels/spk/Voyager_2.m05016u.merged.bsp')
+    spice.furnsh(source_dir + 'voyagers/kernels/spk/sat337.bsp')
+    spice.furnsh(source_dir + 'voyagers/kernels/spk/vgr1_jup230.bsp')
+    spice.furnsh(source_dir + 'voyagers/kernels/spk/vgr1_sat337.bsp')
+    spice.furnsh(source_dir + 'voyagers/kernels/spk/vgr1.x2100.bsp')
+    spice.furnsh(source_dir + 'voyagers/kernels/spk/vgr2_jup230.bsp')
+    spice.furnsh(source_dir + 'voyagers/kernels/spk/vgr2_nep097.bsp')
+    spice.furnsh(source_dir + 'voyagers/kernels/spk/vgr2_sat337.bsp')
+    spice.furnsh(source_dir + 'voyagers/kernels/spk/vgr2.ura182.bsp')
+    spice.furnsh(source_dir + 'voyagers/kernels/spk/vgr2.x2100.bsp')
+
+    # load FK
+    # https://naif.jpl.nasa.gov/pub/naif/pds/data/vco-v-spice-6-v1.0/vcosp_1000/data/fk/
+    spice.furnsh(source_dir + 'voyagers/kernels/fk/vg1_v02.tf')
+    spice.furnsh(source_dir + 'voyagers/kernels/fk/vg2_v02.tf')
+
+    # load LSK 
+    # https://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/
+    spice.furnsh(source_dir + 'generic_kernels/lsk/naif0012.tls')
+
+    # load PCK 
+    # https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/
+    spice.furnsh(source_dir + 'generic_kernels/pck/pck00011.tpc')
+
+    return
